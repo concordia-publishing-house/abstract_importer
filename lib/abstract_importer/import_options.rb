985c3e26cd6bf62c039078d53854bd716b6fefbf
Bob Lail and Gene Doyel <pair=bob.lail+gene.doyel@cphepdev.com>
2015-08-11 10:47:37 -0500

    [refactor] Defined Import callbacks dynamically (5m)

--- lib/abstract_importer/import_options.rb
+++ lib/abstract_importer/import_options.rb
@@ -1,40 +1,22 @@
 module AbstractImporter
   class ImportOptions
-    attr_reader :finder_callback,
-                :rescue_callback,
-                :before_build_callback,
-                :before_create_callback,
-                :after_create_callback,
-                :before_all_callback,
-                :after_all_callback
-                
-    def finder(sym=nil, &block)
-      @finder_callback = sym || block
+    CALLBACKS = [ :finder,
+                  :rescue,
+                  :before_build,
+                  :before_create,
+                  :after_create,
+                  :before_all,
+                  :after_all ]
+
+    CALLBACKS.each do |callback|
+      attr_reader :"#{callback}_callback"
+
+      class_eval <<-RUBY, __FILE__, __LINE__ + 1
+      def #{callback}(sym=nil, &block)
+        @#{callback}_callback = sym || block
+      end
+      RUBY
     end
-    
-    def before_build(sym=nil, &block)
-      @before_build_callback = sym || block
-    end
-    
-    def before_create(sym=nil, &block)
-      @before_create_callback = sym || block
-    end
-    
-    def after_create(sym=nil, &block)
-      @after_create_callback = sym || block
-    end
-    
-    def rescue(sym=nil, &block)
-      @rescue_callback = sym || block
-    end
-    
-    def before_all(sym=nil, &block)
-      @before_all_callback = sym || block
-    end
-    
-    def after_all(sym=nil, &block)
-      @after_all_callback = sym || block
-    end
-    
+
   end
 end
