b717af36a271802e2d5e506bf814f2fdc0c442cd
Bob Lail <bob.lailfamily@gmail.com>
2016-10-26 11:17:58 -0500

    [refactor] Derived UpsertStrategy from InsertStrategy more surgically (12m)

--- lib/abstract_importer/strategies/upsert_strategy.rb
+++ lib/abstract_importer/strategies/upsert_strategy.rb
@@ -6,41 +6,17 @@ module AbstractImporter
     class UpsertStrategy < InsertStrategy
 
 
-      def process_record(hash)
-        summary.total += 1
-
-        remap_foreign_keys!(hash)
-
-        if redundant_record?(hash)
-          summary.redundant += 1
-          return
-        end
-
-        @batch << prepare_attributes(hash)
-        flush if @batch.length >= @batch_size
-
-      rescue ::AbstractImporter::Skip
-        summary.skipped += 1
+      # We won't skip any records for already being imported
+      def already_imported?(hash)
+        false
       end
 
 
-      def flush
-        invoke_callback(:before_batch, @batch)
-
-        collection.scope.insert_many(@batch, on_conflict: {
+      def insert_batch(batch)
+        collection.scope.insert_many(batch, on_conflict: {
           column: remap_ids? ? :legacy_id : :id,
           do: :update
         })
-
-        if remap_ids?
-          id_map.merge! collection.table_name,
-            collection.scope.where(legacy_id: @batch.map { |hash| hash[:legacy_id] })
-        end
-
-        summary.created += @batch.length
-        reporter.batch_inserted(@batch.length)
-
-        @batch = []
       end
 
 
